c83c64f4f109f3e272f2b1b77639825a45bbd09f
amit singh <56405921+amitsingh7668@users.noreply.github.com>
2024-07-24 17:00:05 +0200

    Update query.py

--- query.py
+++ query.py
@@ -1,42 +1,5 @@
-
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-
-public class MetadataService {
-
-    private MetadataRepository metadataRepository;
-
-    public MetadataService(MetadataRepository metadataRepository) {
-        this.metadataRepository = metadataRepository;
-    }
-
-    public CompletableFuture<List<Metadata>> getPendingMetadataByArpRolesAndActorAsync(Set<String> groups, String email) {
-        // Call repository methods asynchronously
-        CompletableFuture<Set<Metadata>> pendingMetadataFuture = CompletableFuture.supplyAsync(() -> 
-            new HashSet<>(metadataRepository.getPendingMetadataldByArpRoles(groups, email))
-        );
-
-        CompletableFuture<Set<Metadata>> actorMetadataFuture = CompletableFuture.supplyAsync(() -> 
-            new HashSet<>(metadataRepository.getActorMetadataIdSByActorEmail(email))
-        );
-
-        // Combine the results
-        return pendingMetadataFuture.thenCombine(actorMetadataFuture, (pendingMetadata, actorMetadata) -> {
-            Set<Metadata> result = new HashSet<>();
-            result.addAll(pendingMetadata);
-            result.addAll(actorMetadata);
-            return new ArrayList<>(result);
-        });
-    }
-
-    public List<Metadata> getPendingMetadataByArpRolesAndActor(Set<String> groups, String email) {
-        try {
-            return getPendingMetadataByArpRolesAndActorAsync(groups, email).get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-}
+java -cp "main/feeds-service-1.11.1-SNAPSHOT.jar:lib/*" \
+     -Djsse.enableSNIExtension=false \
+     -Doracle.jdbc.autoCommitSpecCompliant=false \
+     -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
+     com.ubs.workbench.feeds.WbIntegrator $1 $2 $$ &
